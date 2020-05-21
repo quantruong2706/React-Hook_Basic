adfb2b4c2d9bcd686b2ee14a843452e8352d73b6
quantruong2706 <truongilv3000@gmail.com>
2020-05-21 19:19:11 +0700

    todoList-Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    {id: 1, title: 'Di cho'},
+    {id: 2, title: 'Da bong'},
+    {id: 3, title: 'Xem phim'}
+  ]);
+
+  function handleTodoClick(todo){
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if(index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index,1);
+    setTodoList(newTodoList);
+  }
+  
+  function handleTodoFormSubmit(formValues){
+    console.log('Form submit:', formValues);
+
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    }
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
       Hello every body
+      <TodoForm onSubmit={handleTodoFormSubmit}/>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/TodoForm/index.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+TodoForm.propTypes = {
+    onSubmit: PropTypes.func,
+};
+TodoForm.defaultProps= {
+    onSubmit: null,
+}
+
+function TodoForm(props) {
+    const { onSubmit } = props;
+    const [ value, setValue ] = useState('');
+
+    function handleValueChange(e){
+        console.log(e.target.value);
+        setValue(e.target.value);
+    }
+    function handleSubmit(e){
+        //prevent reload browser
+        e.preventDefault();
+        if(!onSubmit) return;
+        const formValues = {
+            title: value,
+            //should do this, in the future, easy to update
+        }
+        onSubmit(formValues);
+        setValue('');
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={value} onChange={handleValueChange}/>
+        </form>
+    );
+}
+
+export default TodoForm;
